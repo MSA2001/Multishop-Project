a42b1fe52c97c1eb44cb027931e5549c4306aafd
MSA2001 <msa2001b21@gmail.com>
2023-02-02 18:13:23 +0330

    .

---
+++ Account/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+
+
+class UserLoginView(View):
+    def get(self, request):
+        pass
+
--- Mshop/urls.py
+++ Mshop/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Shop.urls'))
+    path('', include('Shop.urls')),
+    path('', include('Account.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
