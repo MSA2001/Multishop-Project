16e2acae41e8db1d502a5db9ca4a3f95eed7fc44
MSA2001 <msa2001b21@gmail.com>
2023-02-08 17:13:48 +0330

    User Login

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserLoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 from django.views import View
@@ -12,3 +14,16 @@ class UserLoginView(View):
         form = self.form_class()
         return render(request, 'Account/login.html', {'form': form})
 
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in successfully', 'success')
+                return redirect('shop:home')
+            else:
+                messages.error(request, 'invalid phone number/password', 'danger')
+                return redirect('Account:user_login')
+        return render(request, 'Account/login.html', {'form': form})
