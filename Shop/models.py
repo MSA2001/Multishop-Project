5be8410b7cc6e8024d05d635efb4afde36217d37
MSA2001 <msa2001b21@gmail.com>
2023-02-13 21:16:47 +0330

    products model 1

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Size(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
+class Color(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    discount = models.SmallIntegerField()
+    image = models.ImageField(upload_to='products')
+    size = models.ManyToManyField(Size, related_name='products')
+    color = models.ManyToManyField(Color, related_name='products')
+
+    def __str__(self):
+        return self.title
