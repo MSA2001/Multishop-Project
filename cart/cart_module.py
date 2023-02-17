09ee8c7afc54f0d9dc3bec9ffa73192f8e9bca94
MSA2001 <msa2001b21@gmail.com>
2023-02-17 16:25:11 +0330

    Cart sessions 3

--- cart/cart_module.py
+++ cart/cart_module.py
@@ -1,4 +1,4 @@
-
+from Shop.models import Product
 
 CART_SESSION_ID = 'cart'
 
@@ -14,15 +14,22 @@ class Cart:
 
         self.cart = cart
 
+    def __iter__(self):
+        cart = self.cart.copy()
+
+        for item in cart.values():
+            item['product'] = Product.objects.get(id=int(item['id']))
+            item['total'] = int(item['price']) * int(item['quantity'])
+            yield item
 
     def unique_id_generator(self, id, color, size):
         result = f'{id}-{color}-{size}'
         return result
 
-    def __add__(self, product, quantity, color, size):
+    def add(self, product, quantity, color, size):
         unique = self.unique_id_generator(product.id, color, size)
         if unique not in self.cart:
-            self.cart[unique] = {'quantity': 0, 'price': str(product.price), 'color': color, 'size': size, 'id': product.id}
+            self.cart[unique] = {'quantity': 0, 'price': str(product.price), 'color': color, 'size': size, 'id': str(product.id)}
 
         self.cart[unique]['quantity'] += int(quantity)
         self.save()
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from .cart_module import Cart
 
 # Create your views here.
 from Shop.models import Product
@@ -7,7 +8,8 @@ from Shop.models import Product
 
 class CartDetailView(View):
     def get(self, request):
-        return render(request, 'cart/cart_detail.html')
+        cart = Cart(request)
+        return render(request, 'cart/cart_detail.html', {'cart': cart})
 
 
 class CartAddView(View):
@@ -15,5 +17,7 @@ class CartAddView(View):
     def post(self, request, pk):
         size, color, quantity = request.POST.get('size'), request.POST.get('color'), request.POST.get('quantity')
         product = get_object_or_404(Product, id=pk)
+        cart = Cart(request)
+        cart.add(product, quantity, color, size)
         print(size, color, quantity)
         return redirect('cart:cart_detail')
