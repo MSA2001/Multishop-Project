f370a4e8f122b041cf975e06dbceb0141373e424
MSA2001 <msa2001b21@gmail.com>
2023-02-18 18:55:15 +0330

    Order models created

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Order, OrderItem
 # Register your models here.
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'phone', 'is_paid')
+    inlines = (OrderItemAdmin,)
+    list_filter = ('is_paid', 'created_at')
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from Account.models import User
+from Shop.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    address = models.CharField(max_length=350)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.phone
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    size = models.CharField(max_length=3)
+    color = models.CharField(max_length=12)
+    quantity = models.SmallIntegerField()
+    price = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.order.phone
+
